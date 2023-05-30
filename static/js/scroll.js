f1e11c2f72017ccf557216087795cb9cdf6ddc79
Theo Baur <baurt@cardiff.ac.uk>
2023-05-30 14:32:46 +0100

    fixed scrolling animation for work experience

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -14,9 +14,9 @@ window.addEventListener('scroll', function(element) {
         element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
     });
 
-    const target3 = document.querySelectorAll('.work-experience');
+    const target3 = document.querySelectorAll('.work-experience-section');
     target3.forEach(function(element) {
-        var pos = window.pageYOffset-650;
+        var pos = window.pageYOffset-2000;
         var rate = pos * -0.07;
         element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
     });
