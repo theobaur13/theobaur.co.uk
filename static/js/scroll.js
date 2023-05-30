b2d79dce077d7d1c03c4cbf1cbfce85b5799006f
Theo Baur <baurt@cardiff.ac.uk>
2023-05-30 17:06:09 +0100

    fixed strange layout when scrolling out

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,25 +1,42 @@
 window.addEventListener('scroll', function(element) {
     const target = document.querySelectorAll('.scroll');
-    target.forEach(function(element) {
-        var pos = window.pageYOffset-872;
-        var rate = pos * 0.07;
-        element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
-        element.style.transform = 'scale(rate)';
-    });
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-    const target2 = document.querySelectorAll('.project');
-    target2.forEach(function(element) {
-        var pos = window.pageYOffset-872;
-        var rate = pos * -0.07;
-        element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
-    });
+    if (screenWidth < 2160) {
+        target.forEach(function(element) {
+            var pos = window.pageYOffset-872;
+            var rate = pos * 0.07;
+            element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
+            element.style.transform = 'scale(rate)';
+        });
 
-    const target3 = document.querySelectorAll('.work-experience-section');
-    target3.forEach(function(element) {
-        var pos = window.pageYOffset-2000;
-        var rate = pos * -0.07;
-        element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
-    });
+        const target2 = document.querySelectorAll('.project');
+        target2.forEach(function(element) {
+            var pos = window.pageYOffset-872;
+            var rate = pos * -0.07;
+            element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
+        });
 
-    console.log(window.pageYOffset);
+        const target3 = document.querySelectorAll('.work-experience-section');
+        target3.forEach(function(element) {
+            var pos = window.pageYOffset-2000;
+            var rate = pos * -0.07;
+            element.style.transform = 'translate3d(0px, '+rate+'px, 0px)';
+        }); 
+    }
+    else {
+        target.forEach(function(element) {
+            element.style.transform = 'translate3d(0px, 0px, 0px)';
+        });
+
+        const target2 = document.querySelectorAll('.project');
+        target2.forEach(function(element) {
+            element.style.transform = 'translate3d(0px, 0px, 0px)';
+        });
+
+        const target3 = document.querySelectorAll('.work-experience-section');
+        target3.forEach(function(element) {
+            element.style.transform = 'translate3d(0px, 0px, 0px)';
+        });
+    }
 });
